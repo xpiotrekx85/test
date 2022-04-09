faa22112a66edbc92a869929fbd71aa1d8fe1f9f
xpiotrekx85 <oprzala@gmail.com>
2022-04-09 11:42:09 +0200

    Po pracy domowej

---
+++ src/Calculator.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("podaj pierwsza liczbę: ");
+        int pierwszaLiczba = scanner.nextInt();
+        System.out.println("podaj drugą liczbę: ");
+        int drugaLiczba = scanner.nextInt();
+        System.out.println("Wynik mnożenia:  " + pierwszaLiczba + " * " + drugaLiczba + " = " + (pierwszaLiczba * drugaLiczba));
+        System.out.println("Wynik odejmowania: " + drugaLiczba + " - " + pierwszaLiczba + " = " + (drugaLiczba - pierwszaLiczba));
+
+
+    }
+}
