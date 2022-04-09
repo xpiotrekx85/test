52b1af4537b00879716f98943b10b6111dc2eb50
xpiotrekx85 <oprzala@gmail.com>
2022-04-09 16:56:49 +0200

    Petle while

---
+++ src/PetleWhile.java
@@ -0,0 +1,11 @@
+public class PetleWhile {
+    public static void main(String[] args) {
+        int numer = -100;
+        while(numer < 20) {
+            System.out.println("Moja wartosc to: " + numer + " i jestem mniejszy od 20");
+            numer++;
+
+        }
+
+    }
+}
